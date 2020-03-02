113915594d85e792ae7895843e1b3d9dab0576d4
Ben Harris <benjaminharris28@gmail.com>
2020-03-01 23:22:20 -0500

    updated to use working protected endpoints and auth

---
+++ src/middleware/basic-auth.js
@@ -0,0 +1,45 @@
+const AuthService = require('../auth/auth-service')
+
+function requireAuth(req, res, next) {
+    const authToken = req.get('Authorization') || ''
+    
+    let basicToken
+
+    // Conditional if authtoken is missing
+    if (!authToken.toLowerCase().startsWith('basic ')) {
+        return res.status(401).json({ error: 'Missing basic token' })
+    } else {
+        basicToken = authToken.slice('basic '.length, authToken.length)
+    }
+
+    // What does Buffer do?
+    // Parse token value out of header
+    const [tokenUserName, tokenPassword] = AuthService.parseBasicToken(basicToken)
+
+    // Check if username or password is missing and throw error
+    if (!tokenUserName || !tokenPassword) {
+        return res.status(401).json({ error: 'Unauthorized request'})
+    }
+
+    // Query database for a user that matches this username
+    
+    //QUESTION: I've never seen syntax with back to back brackets before
+    AuthService.getUserWithUserName(
+        req.app.get('db'),
+        tokenUserName
+    )
+    
+        .then(user => {
+            if (!user || user.password !== tokenPassword) {
+                return res.status(401).json({ error: 'Unauthorized request' })
+            }
+            req.user = user
+            next()
+        })
+
+    .catch(next)
+  }
+  
+  module.exports = {
+    requireAuth,
+  }
